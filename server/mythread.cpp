ce3b8dd8a2ed441278c4f8668ebbbc820d55a41f
Your Name <you@example.com>
2017-12-03 11:23:13 +0300

    Явное использование перечисления команд

--- server/mythread.cpp
+++ server/mythread.cpp
@@ -1,132 +1,132 @@
 #include "mythread.h"
 
 MyThread::MyThread(int ID, QObject* parent)
-    : QThread(parent)
+	: QThread(parent)
 {
-    this->socketDescriptor = ID;
+	this->socketDescriptor = ID;
 }
 
 void MyThread::run()
 {
-    qDebug() << socketDescriptor << "Starting thread";
-    pSocket = new QTcpSocket();
-    if (!pSocket->setSocketDescriptor(this->socketDescriptor))
-    {
-        emit error(pSocket->error());
-        return;
-    }
+	qDebug() << socketDescriptor << "Starting thread";
+	pSocket = new QTcpSocket();
+	if (!pSocket->setSocketDescriptor(this->socketDescriptor))
+	{
+		emit error(pSocket->error());
+		return;
+	}
 
-    connect(pSocket, SIGNAL(readyRead()), this, SLOT(readyRead()), Qt::DirectConnection);
-    connect(pSocket, SIGNAL(disconnected()), this, SLOT(disconnected()), Qt::DirectConnection);
+	connect(pSocket, SIGNAL(readyRead()), this, SLOT(readyRead()), Qt::DirectConnection);
+	connect(pSocket, SIGNAL(disconnected()), this, SLOT(disconnected()), Qt::DirectConnection);
 
-    sendString("Please, enter your name.");
-    c = (Command)0;
+	sendString("Please, enter your name.");
+	c = Command::AskLogin;
 
-    qDebug() << socketDescriptor << "Client Connected";
+	qDebug() << socketDescriptor << "Client Connected";
 
-    exec();
+	exec();
 }
 
 void MyThread::readyRead()
 {
-    QString data;
-    QStringList myOptions;
-    myOptions << "-help"
-              << "-players list"
-              << "-create lobby"
-              << "-lobbies list";
+	QString data;
+	QStringList myOptions;
+	myOptions << "-help"
+			  << "-players list"
+			  << "-create lobby"
+			  << "-lobbies list";
 
-    QDataStream in(pSocket);
-    in.setVersion(QDataStream::Qt_4_0);
-    in >> data;
+	QDataStream in(pSocket);
+	in.setVersion(QDataStream::Qt_4_0);
+	in >> data;
 
-    QString lobbyName = "ErrorLobbyName";
-    if (data.contains("-create lobby"))
-    {
-        lobbyName = data;
-        lobbyName.remove(0, 14);
-        data = "-create lobby";
-        qDebug() << lobbyName;
-    }
+	QString lobbyName = "ErrorLobbyName";
+	if (data.contains("-create lobby"))
+	{
+		lobbyName = data;
+		lobbyName.remove(0, 14);
+		data = "-create lobby";
+		qDebug() << lobbyName;
+	}
 
-    qDebug() << data;
+	qDebug() << data;
 
-    if (c != (Command)0)
-    {
-        switch (myOptions.indexOf(data))
-        {
-        case 0: // Help
-            c = (Command)1;
-            break;
-        case 1: // AskPlayers
-            c = (Command)2;
-            break;
-        case 2: // CreateLobby
-            c = (Command)3;
-            break;
-        case 3: // AskLobby
-            c = (Command)4;
-            break;
-        default: // WrongCommand
-            c = (Command)5;
-            sendString("Wrong command '" + data + "'.");
-            break;
-        }
-    }
+	if (c != Command::AskLogin)
+	{
+		switch (myOptions.indexOf(data))
+		{
+		case 0:
+			c = Command::Help;
+			break;
+		case 1:
+			c = Command::AskPlayers;
+			break;
+		case 2:
+			c = Command::CreateLobby;
+			break;
+		case 3:
+			c = Command::AskLobby;
+			break;
+		default:
+			c = Command::WrongCommand;
+			sendString("Wrong command '" + data + "'.");
+			break;
+		}
+	}
 
-    switch (c)
-    {
-    case AskLogin:
-        login(data);
-        break;
-    case Help:
-        help();
-        break;
-    case AskPlayers:
-        emit sendPlayerList(this);
-        break;
-    case CreateLobby:
-        emit createLobby(this, lobbyName);
-        break;
-    case AskLobbies:
-        emit sendLobbiesList(this);
-        break;
-    case WrongCommand:
-        break;
-    }
+	switch (c)
+	{
+	case AskLogin:
+		login(data);
+		break;
+	case Help:
+		help();
+		break;
+	case AskPlayers:
+		emit sendPlayerList(this);
+		break;
+	case CreateLobby:
+		emit createLobby(this, lobbyName);
+		break;
+	case AskLobbies:
+		emit sendLobbiesList(this);
+		break;
+	case WrongCommand:
+		break;
+	}
 }
 
 void MyThread::login(QString login)
 {
-    player = new Player(login, socketDescriptor);
-    sendString("Hello, " + login + "! Type -help to ask for available commands");
-    c = (Command)1;
+	player = new Player(login, socketDescriptor);
+	sendString("Hello, " + login + "! Type -help to ask for available commands");
+	c = Command::Help;
 }
 
 void MyThread::help()
 {
-    sendString("-help: ask for available commands\n-players list: ask for player list\n-create lobby 'LobbyName': ask for creating a lobby\n-lobbies list: ask for lobbies list\n");
+	sendString("-help: ask for available commands\n-players list: ask for player list\n-create lobby 'LobbyName': ask for creating a lobby\n-lobbies list: ask for lobbies list\n");
 }
 
 /*void MyThread::playerList()
 {
-    while (!players.empty())
-        sendString(players.back()->name + '/n');
-    players.pop_back();
-    sendString("only uuuuuuuuuuuuuuuuu");
+	while (!players.empty())
+		sendString(players.back()->name + '/n');
+	players.pop_back();
+	sendString("only uuuuuuuuuuuuuuuuu");
 }*/
 
 void MyThread::disconnected()
 {
-    qDebug() << socketDescriptor << "Client Disconnected";
-    pSocket->deleteLater();
-    exit(0);
+	qDebug() << socketDescriptor << "Client Disconnected";
+	pSocket->deleteLater();
+	exit(0);
 }
 
 void MyThread::sendString(QString message)
 {
-    message += "\n";
-    QByteArray buffer(message.toStdString().c_str());
-    pSocket->write(buffer);
-    pSocket->waitForBytesWritten();
+	message += "\n";
+	QByteArray buffer(message.toStdString().c_str());
+	pSocket->write(buffer);
+	pSocket->waitForBytesWritten();
 }
