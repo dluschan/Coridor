f9856417447f62644e3c7c06e9f603d96f0e5f35
Your Name <you@example.com>
2018-01-04 15:47:43 +0300

    Changed client from form using to self creating widgets

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -1,35 +1,97 @@
 #include "mainwindow.h"
 #include "../common/command.h"
-#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget* parent)
 	: QMainWindow(parent)
-	, ui(new Ui::MainWindow)
 {
-	ui->setupUi(this);
-	// ui->Enter->setDefault(true);
+	resize(400, 300);
+	setMinimumWidth(400);
+	setMinimumHeight(300);
+	setWindowTitle("Client");
+
+	centralWidget = new QWidget(this);
+	setCentralWidget(centralWidget);
+
 	pSocket = new QTcpSocket(this);
 	connect(pSocket, SIGNAL(readyRead()), this, SLOT(sockReady()));
 	connect(pSocket, SIGNAL(disconnected()), this, SLOT(sockDisc()));
-	connect(this, SIGNAL(signalLabelUpdate()), this, SLOT(slotLabelUpdate()));
-	connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(connectToTheServer()));
-	// connect(ui->Enter, SIGNAL(clicked()), this, SLOT(sendMessage()));
-	connect(ui->Enter, SIGNAL(clicked()), this, SLOT(sendCommand()));
+
+	connectLayout = new QVBoxLayout(centralWidget);
+	connectBtn = new QPushButton("Connect to server");
+	loginEdit = new QLineEdit();
+	passwordEdit = new QLineEdit();
+	connectLayout->addWidget(loginEdit);
+	connectLayout->addWidget(passwordEdit);
+	connectLayout->addWidget(connectBtn);
+	connect(connectBtn, SIGNAL(clicked()), this, SLOT(connectToTheServer()));
 }
 
 MainWindow::~MainWindow()
 {
-	delete ui;
 }
 
 void MainWindow::connectToTheServer()
 {
 	pSocket->connectToHost("127.0.0.1", 5555);
+
+	QByteArray arrBlock;
+	QDataStream out(&arrBlock, QIODevice::WriteOnly);
+	out.setVersion(QDataStream::Qt_5_9);
+
+	CommandType commandType = {0};
+	Command* pCommand = new Login(loginEdit->text());
+
+	out << commandType;
+	pCommand->operator<<(out);
+	pSocket->write(arrBlock);
+	pSocket->waitForBytesWritten();
+
+	qDebug() << "Command Sent";
+
+	switchToMain();
+}
+
+void MainWindow::createLobbyDialog()
+{
+	CreateLobbyDialog dialog();
+	qDebug() << "obey";
+}
+
+void MainWindow::switchToLoginIn()
+{
+	centralWidget = new QWidget(this);
+	setCentralWidget(centralWidget);
+
+	connectLayout = new QVBoxLayout(centralWidget);
+	connectBtn = new QPushButton("Connect to server");
+	loginEdit = new QLineEdit();
+	passwordEdit = new QLineEdit();
+	connectLayout->addWidget(loginEdit);
+	connectLayout->addWidget(passwordEdit);
+	connectLayout->addWidget(connectBtn);
+	connect(connectBtn, SIGNAL(clicked()), this, SLOT(connectToTheServer()));
+}
+
+void MainWindow::switchToMain()
+{
+	// delete centralWidget->layout();
+	centralWidget = new QWidget(this);
+	setCentralWidget(centralWidget);
+	mainLayout = new QVBoxLayout(centralWidget);
+	createLobbyBtn = new QPushButton("Create Lobby");
+	showLobbiesBtn = new QPushButton("Connect to Lobby");
+	disconnectBtn = new QPushButton("Log out");
+	mainLayout->addWidget(createLobbyBtn);
+	mainLayout->addWidget(showLobbiesBtn);
+	mainLayout->addWidget(disconnectBtn);
+	connect(disconnectBtn, SIGNAL(clicked()), this, SLOT(sockDisc()));
+	connect(createLobbyBtn, SIGNAL(clicked()), this, SLOT(createLobbyDialog()));
 }
 
 void MainWindow::sockDisc()
 {
 	pSocket->deleteLater();
+	switchToLoginIn();
 }
 
 void MainWindow::sockReady()
@@ -39,44 +101,19 @@ void MainWindow::sockReady()
 		pSocket->waitForReadyRead(500);
 		data = pSocket->readAll();
 		qDebug() << data;
-		emit signalLabelUpdate();
 	}
 }
 
-void MainWindow::slotLabelUpdate()
-{
-	ui->label->setText(QString(data));
-}
-
-void MainWindow::sendMessage()
+/*void MainWindow::sendMessage()
 {
 	QByteArray arrBlock;
 	QDataStream out(&arrBlock, QIODevice::WriteOnly);
-	out.setVersion(QDataStream::Qt_5_10);
-	out << ui->lineEdit->text();
+	out.setVersion(QDataStream::Qt_5_9);
+	out << lineEdit->text();
 	pSocket->write(arrBlock);
 	pSocket->waitForBytesWritten();
 
-	// ui->lineEdit->setText("");
+	// lineEdit->setText("");
 
 	qDebug() << "Message Sent";
-}
-
-void MainWindow::sendCommand()
-{
-	QByteArray arrBlock;
-	QDataStream out(&arrBlock, QIODevice::WriteOnly);
-	out.setVersion(QDataStream::Qt_5_10);
-
-	CommandType commandType = {0};
-	Command* pCommand = new Login(ui->lineEdit->text());
-
-	out << commandType;
-	pCommand->operator<<(out);
-	pSocket->write(arrBlock);
-	pSocket->waitForBytesWritten();
-
-	ui->lineEdit->setText("");
-
-	qDebug() << "Command Sent";
-}
+}*/
